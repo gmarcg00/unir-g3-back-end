757ea2b2995607186af4668843a4d1c8192e7b10
electrognnu <electro.gnu@gmail.com>
2024-11-23 00:41:26 +0000

    Update AdminInfoResponse.js cambio "role_id"

--- src/controllers/models/AdminInfoResponse.js
+++ src/controllers/models/AdminInfoResponse.js
@@ -7,7 +7,7 @@ class AdminInfoResponse {
         this.email = user.email;
         this.username = user.username;
         this.image = user.image;
-        this.role = user.roles_id;
+        this.role = user.role_id;
     }
 
 }
