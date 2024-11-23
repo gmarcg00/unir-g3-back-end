aab86196bc9df27338853eb4b8d93254f74fb256
Guillermo Marcos <gmarcg00@estudiantes.unileon.es>
2024-11-23 01:01:58 +0100

    endpoint implementado

--- src/models/KnowledgeBranchModel.js
+++ src/models/KnowledgeBranchModel.js
@@ -1,8 +1,11 @@
 const pool = require("../config/db");
 
-async findBy
+async function findAll(){
+    const [result] = await pool.query('SELECT * FROM knowledge_branches');
+    return result;
+}
 
 
 module.exports = {
-
+    findAll
 }
