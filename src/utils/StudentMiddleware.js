6ddcbba569ef1f2cce2145a00bdc0ea945159b84
Guillermo Marcos <gmarcg00@estudiantes.unileon.es>
2024-12-07 01:49:48 +0100

    comentarios eliminados

--- src/utils/StudentMiddleware.js
+++ src/utils/StudentMiddleware.js
@@ -1,14 +1,10 @@
 
 const checkStudentRatesTeacherPayload= async (req, res, next) => {
     const {rating, text_rating} = req.body;
-    //const student_id = req.params.student_id;
-    //const teacher_id = req.params.teacher_id;
-
-    //if (!student_id || !teacher_id || !rating || !text_rating ) {
-        if ( !rating || !text_rating ) {
+    
+    if ( !rating || !text_rating ) {
         return res.status(400).json({
             code: 'BAD_REQUEST',
-            //message: 'The request params must contain the fields student_id and teacher_id, and the request body must contain the fields: rating and text_rating'
             message: 'The request body must contain the fields: rating and text_rating'
         });
     }
